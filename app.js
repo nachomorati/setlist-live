9d94c60fe34b6f685d10ae70a5b9b1a0b74b3005
nachomorati <martincho8a@gmail.com>
2026-06-29 23:16:07 -0300

    Update canciones en app.js

--- app.js
+++ app.js
@@ -2,10 +2,94 @@
 
 let canciones = JSON.parse(localStorage.getItem('setlist_canciones')) || [
     {
-        id: 1,
-        titulo: "Cheques",
-        duracion: 240,
-        letra: "Ella utiliza mis cheques de viajero...\n\n[pause=15]\n(Solo de Guitarra - Scroll Pausado)\n\nQuiero verla de nuevo."
+        "id": 1782780103781,
+        "titulo": "Un pacto",
+        "duracion": 400,
+        "letra": "Un pacto para vivir\nOdiándonos sol a sol\nRevolviendo más\nEn los restos de un amor\nCon un camino recto\nA la desesperación\nDesenlace en un cuento de terror\n\nSeis años así\nEscapando a un mismo lugar\nCon mi fantasía\nBuscando otro cuerpo, otra voz\nFui consumiendo infiernos\nPara salir de vos\nIntoxicado, loco y sin humor\n\nSi hoy te tuviera aquí\nCuando hago esta canción\nMe sentiría raro\nNo tengo sueño, mi panza vibra\nTuve un golpe energético\nMilagro y resurreción\nY eso que estaba tieso\nY bajo control\n\nEl poder siempre manda\nSi para tenerte aquí\nHabría que maltratarte\nNo puedo hacerlo, sos mi Dios\nTe veo, me sonrojo y tiemblo\nQué idiota te hace el amor\nY hoy quiero darle rienda\nA esta superstición\n\nUn pacto para vivir\n(Un pacto para vivir, un pacto para vivir)\nUn pacto para vivir\n(Un pacto para vivir, un pacto para vivir)\nUn pacto para vivir\n(Un pacto para vivir, un pacto para vivir)\n\nUn pacto para vivir\nPara vivir"
+    },
+    {
+        "id": 1782780505066,
+        "titulo": "Un angel para tu soledad",
+        "duracion": 150,
+        "letra": "[pause=20]\n...\n\nYa sufriste cosas mejores que éstas\ny vas a andar esta ruta hoy\ncuando anochezca\n\n\n...\nTu esqueleto te trajo hasta aquí\ncon un cuerpo hambriento veloz\ny aquí gracias a Dios\nuno no cree en lo que oye\n\n\n...\nÁngel de la soledad\ny de la desolación\npreso de tu ilusión\nvas a bailar a bailar bailar\n\n...\nEs tan simple así\nno podes elegir\nclaro que no siempre\n¿Ves?, resulta bien\n\n...\nAtado con doble cordel\nel de simular\nno querés girar maniatado\nqerés faulear y arremolinar.\n\n[pause=10]\n\nMedís tu acrobacia y saltás\ntu secreto es\nla suerte del principiante\nno puede fallar\n\n...\nAlguna vez quizá\nse te va la mano\ny las llamas en pena invaden tu cuerpo\n...\nY caés en manos del\nÁngel de la soledad\ny él gracias a Dios\ntampoco cree en lo que oye\n\n...\nÁngel de la soledad\ny de la desolación\npreso de tu ilusión\nvas a bailar a bailar bailar\n\n...\nPor mis penas bailá\ny por tu soledad."
+    },
+    {
+        "id": 1782780157333,
+        "titulo": "Rezo por vos",
+        "duracion": 300,
+        "letra": "La indómita luz\nSe hizo carne en mí\nY lo dejé todo por esta soledad\nY leo revistas\nEn la tempestad\nHice el sacrificio\nAbracé la cruz al amanecer\n\nRezo\nRezo\nRezo\nRezo\n\nMorí sin morir\nY me abracé al dolor\nY lo dejé todo por esta soledad\nYa se hizo de noche\nY ahora estoy aquí\nMi cuerpo se cae\nSolo veo la cruz al amanecer\n\nRezo (rezo)\nRezo (rezo)\nRezo (rezo)\nRezo por vos (rezo)\n\nY curé mis heridas\nY me encendí de amor\nY quemé las cortinas\nY me encendí de amor\nDe amor sagrado\n\nY entonces\nY entonces\nY entonces\nY entonces\nY entonces\nY entonces\n\nY curé mis heridas\nY me encendí de amor\nY quemé las cortinas\nY me encendí de amor\nDe amor sagrado\n\nRezo\nRezo\nRezo\nRezo\nRezo\nRezo por vos (rezo)"
+    },
+    {
+        "id": 1782780644041,
+        "titulo": "Quieren rock",
+        "duracion": 300,
+        "letra": "Los chicos saben dónde está la acción\nSe están preparando mientras baja el Sol\nLa banda que les gusta se presenta hoy, uh\nChicos y chicas quieren rock, quieren rock\n\nY quieren rock\nRock\nRock\nQuieren rock\n\nSubo al escenario y veo de repente, uh\nChicos intoxicados por el rock and roll, sí\nYa colgaron sus banderas y cantan como siempre\nQue salga la banda que queremos rock, queremos rock\nQueremos rock\nRock\nRock\nRock\nRock, rock, rock, auh\n\nLos chicos saben dónde está la acción, uh\nSe están preparando mientras baja el Sol\nLa banda que les gusta se presenta hoy, uh\nChicos y chicas quieren rock, quieren rock\nQuieren, quieren, quieren rock\n\nRock\nQuieren rock\nQuieren rock\nPiden rock\nPiden rock\nLes gusta el rock\nQuieren rock\n\nQuieren, quieren, quieren rock\nRock\n(Rock) oh, les gusta el rock (rock)\nQuieren rock\nQuieren rock\nLes gusta el rock\nLes gusta el rock, les gusta el rock, uh\nA los chicos les gusta el rock\nRock, rock, rock, rock, rock, rock, auh"
+    },
+    {
+        "id": 1782780203397,
+        "titulo": "Esperando nacer",
+        "duracion": 300,
+        "letra": "[pause=50]\nTengo el corazón abierto\nTodo el mundo puede ver un camino para correr\nTengo el alma en un desierto\nTodo el mundo puede ser un camino para crecer\n...\nTodo el mundo dice que mi amor es en vano\nY que llevo siglos esperando nacer\nEsperando nacer\n\n...\nYo te he visto en el pasado\nCon tu cara de jarrón y tu mundo hecho de clisés\n¿Qué esperás ahí al costado del camino?\nNo escuchaste esta canción\nTodo el mundo te quiere ver\n...\nTengo la esperanza de encontrar un sonido\nY un amor tan grande que te pueda mover\nPor Dios escúchame\n[pause=20]\n...\nTe imaginas el lamento\nDe la gente y su manual de las cosas\nQue nunca fueron\nY el olor de los jazmines viejos\nY la mustia sensación de que\nEl tiempo se echó a perder\n...\nSoy un solitario transmitiendo un mensaje\nEscribiendo frases para poder creer\nEsperando nacer\nEsperando nacer\nEsperando nacer\nEsperando nacer"
+    },
+    {
+        "id": 1782780291611,
+        "titulo": "Sucio y Desprolijo",
+        "duracion": 340,
+        "letra": "Todas las mañanas son iguales\nlindas, novedosas, especiales\nsiguen reprochando memorales\ntodo lo que yo hago esta mal\n\nSon muchos pensamientos\npara una sola cosa\nestoy algo cansado de vivir,\nen realidad\n\nyo que soy un hombre desprolijo\nno tengo conflictos con mi ser\nporque en la apariencia no me fijo\npiensan que así no puedo ser\nNo cambia nada estar un poco sucio\nsi mi cabeza es eficaz.\n\nno, no, no\n\nyo que soy un hombre desprolijo\nno tengo conflictos con mi ser\nporque en la apariencia no me fijo\npiensan que así no puedo ser\n\nSon muchos pensamientos\npara una sola cosa\nestoy algo cansado de vivir,\nen realidad\n\nNo cambia nada estar un poco sucio\nsi mi cabeza es eficaz.\nsi mi cabeza es eficaz.\nsi mi cabeza es eficaz."
+    },
+    {
+        "id": 1782780684321,
+        "titulo": "Siguiendo la luna",
+        "duracion": 300,
+        "letra": "Siguiendo la luna\nNo llegaré lejos\nTan lejos como se pueda llegar\nLas cosas que dije\nNo tienen sentido\nNo puedo detenerme\nPonerme a pensar\n\nSiguiendo la luna\nY su vuelta invisible\nLa noche seguro que me alcanzará\nNo es que tu mirada\nMe sea imposible\nTan solo es la forma en que caminas\n\nVamos mi cariño que todo está bien\nEsta noche cambiaré\nTe juro que cambiaré\nVamos mi cariño ya no llores más\nPor vos yo bajaría el sol\nO me hundiría en el mar\n\nY esto parece verdad para mí\n\nSuena como un crime\nLo que tu me has hecho\nDeberías ir a parar la prisión\nSuena como un crimen\nQue me hayas mentido\nQue hayas engañado a este corazón\n\nSiguiendo la luna\nNo llegaré lejos\nTan lejos como se pueda llegar\nSon casi las 4 de la madrugada\nMi casa brillaba cruzando ese mar\n\nVamos mi cariño que todo está bien\nEsta noche cambiaré\nTe juro que cambiaré\nVamos mi cariño ya no llores más\nPor vos yo bajaría el sol\nO me hundiría en el mar\n\nY esto parece verdad para mí\nY esto parece verdad para mí\nY esto parece verdad para mí\nY esto parece verdad para mí\n\nY esto parece verdad para mí\nY esto parece verdad para mí, para mi\nY esto parece verdad para mí, uh\nY esto parece verdad para mí, para mí, para mi, para mí\n\nTodo para mi\nVerdad para mí"
+    },
+    {
+        "id": 1782780449434,
+        "titulo": "En la cocina, huevos",
+        "duracion": 300,
+        "letra": "Bachillerato pedagógico\ny el carnet de un club vecino\nuna estampita por las dudas\nque el novio se pase de vivo\n\nLa imagen de mamá y papá en cama\nUn poster de Jagger\nUn Cristo retratado\nharto de estar colgado\nEl sueño eterno de un marido fiel\no que al menos le haga bien el verso\nLa desgracia de la tía Inés\nque a los dieciséis\nse fue con un marinero griego\n\nEl juego eterno de: Ya no me toques\ny por dentro te estas muriendo\nEl juego de: Ya no me toques\nNena vas a ver cuando esta noche\nllegue a casa papá\n\nHuevos, en la cocina hacen falta huevos\nHuevos, el mundo es tan atroz\nHuevos, en la cocina hacen falta huevos\n\nYo sé que a pesar de todo\nla lucha es desigual\nHoy te convocan a la plaza\ny mañana te la dan...\n\nSi pasan música nacional\nno es que se hayan dado cuenta\nque la cultura de un país\nesta en su gente\ny yo sí que aquí hay polenta\nNena vos creías que con la B.C.G.\nse acabaría el drama de tus días\nla vida es algo más, la vida es algo más\n\nHuevos, en la cocina hacen falta huevos\nHuevos, en la cocina hacen falta huevos\nHuevos, huevos, huevos"
+    },
+    {
+        "id": 1782780329699,
+        "titulo": "La excepcion",
+        "duracion": 300,
+        "letra": "Quiero ser lo que te hace más feliz\nA mí me gusta verte así\nComo el fin de este viaje\n\nPréndeme\n¡Sácate!\n\nLlévame a un lugar con parlantes\n\nY que nos vuele la sonoridad\nPor el aire\nUn espacio para celebrar\nSé que esto es grande\n\nEste amor es\nLa mejor cara del poder\nY estás cansada de poner\nEl dedo en la llaga\n\nEste tiempo llegó para ahogar\nA la bestia\nTodo acaba bloqueado\nEntre tanta histeria\n\nHoy hagamos la excepción\nDe romper las reglas\nTanta hambre sin satisfacción, satisfacción\nHoy hagamos la excepción de estirar la cuerda\nY que durar sea\nMejor que arder, mejor que arder\n\n(Nena, llévame a algún lugar con parlantes, vas a ver)\n\nMecanismos de ilusión\nTodo el mundo nena, es impostor\n\nHoy hagamos la excepción de romper las reglas\nTanta hambre sin satisfacción, satisfacción\nHoy hagamos la excepción de estirar la cuerda\nQue durar sea mejor que arder, oh\nUh, uh, uh\nYa rompí las reglas"
+    },
+    {
+        "id": 1782780572018,
+        "titulo": "Hola, que tal?",
+        "duracion": 300,
+        "letra": "Hola, ¿qué tal? ¿Cómo estás?\nDe nuevo aquí para encontrarnos\nNo escucho bien lo que decís\nPero reís para afirmarlo\nTe queda bien lo que llevás\nTe maquillás como con barro\nHoy me gustás más que ayer\nY mañana serás pasado\n\nHola, ¿qué tal? ¿Cómo estás?\nTodos los gestos van ajustados\nMirate a vos, mirame a mí\nSi parecemos como estancados\nNo te pasás más de ahí\nNo te pasás de lo marcado\nBuscás error en los demás\nY vos ya sos un mejorado\n\nHola, ¿qué tal?\nHola, ¿qué tal?\nHola, ¿qué tal?"
+    },
+    {
+        "id": 1782780405955,
+        "titulo": "Juegos de seduccion",
+        "duracion": 300,
+        "letra": "Voy a ser tu mayordomo\nY vos harás el rol de señora bien\nO puedo ser tu violador\nLa imaginación esta noche todo lo puede\n\nTe llevaré hasta el extremo\nTe llevaré (eh, eh, eh)\nAbrázame\nEste es el juego de seducción\n\nEstamos solos en la selva\nY nadie puede venir a rescatarnos\nEstoy muriéndome de sed\nY es tu propia piel la que me hace sentir este infierno\n\nTe llevaré hasta el extremo\nTe llevaré (eh, eh, eh)\nAbrázame\nEste es el juego de seducción\n\nEstoy muriéndome de sed\nY es tu propia piel la que me hace mover\nMe hace mover, me hace mover\nEn extremos"
+    },
+    {
+        "id": 1782779926630,
+        "titulo": "Cheques",
+        "duracion": 308,
+        "letra": "Casualmente yo te conoci\nuna noche como hoy\ntu vestido electrico me hirio\nvos bailabas y decias\nshake it, shake it, shake it, shake it baby.\n\nLuego te mudaste con tu sequito\na mi lujosa mansion\nhasta casi entro en tu narcogate\nvos bailabas y decias\nshake it, shake it, shake it, shake it baby.\n\nDe nuestro amor tan solo me queda hoy\nun absurdo secador\nme fundiste hasta el BMW\ny yo lo que hago es garpar cheques\ncheques, cheques, cheques, cheques"
+    },
+    {
+        "id": 1782780042286,
+        "titulo": "Sasha, Sissi y el circulo de baba",
+        "duracion": 314,
+        "letra": "Pensar que no dijo nada\nLa mañana que lo ví\nPensar que él amaba tanto\nA su divina Sissí\n\nBajé el callejón con calma\nMás no pude resistir\nEntonces recé por Sasha\nPor su amada y por mí\n\nReptar, reptar\nEn horas de la siesta\nCambiar, cambiar\nLas leyes del amar\n\nSissí lo volvió loco en una fiesta\nY no volió a tocarlo nunca más\nEntonces pensó en un arma\nEn matarla y en huir\n\nLloró y escribió su requiem\nPara Sasha y Sissí\nNo hizo más que esperarla\nLa beso y durmió al llegar\n\nY trazó un círculo de baba\nCon un medical style\nY no dejó que nunca más sufriera\nSissí murió de hambre y vanidad\n\nAmar, amar y tuvo muerte lenta\nAsí el meó tres veces mis zapatos y llegó hasta el bar\nSimplemente fue lo que pasó bajo esta luz\nSasha prendió un faso y se sentó\nPitó ese cigarrillo hasta explotar"
+    },
+    {
+        "id": 1,
+        "titulo": "Himno de mi corazon",
+        "duracion": 300,
+        "letra": "Sobre la palma de mi lengua\nVive el himno de mi corazón\nSiento la alianza más perfecta\nQue, en justicia, me une a vos\n\nLa vida es un libro útil\nPara aquel que puede comprender\nTengo confianza en la balanza\nQue inclina mi parecer\n\nNadie quiere dormirse aquí\nAlgo puedo hacer\nTras haber cruzado la mar\nTe seduciré\nPor felicidad, yo canto\n\nNada me abruma ni me impide\nEn este día que te quiera, amor\nNaturalmente, mi presente busca\nFlorecer de a dos\n\nNada hay que nada prohíba\nYa te veo andar en libertad\nQue no se rasgue como seda\nEl clima de tu corazón\n\nNadie quiere dormirse aquí\nAlgo debo hacer\nTras haber cruzado la mar\nTe seduciré\nSolo por amor, lo canto"
+    },
+    {
+        "id": 1782780618257,
+        "titulo": "Me gustas mucho",
+        "duracion": 300,
+        "letra": "Me gustas mucho\nMe gustas tanto que ni te imaginas\nMe gustas mucho\nQue soy capaz de darte mi vida\n\nSacrificarme por un beso tuyo\nSería un honor si estás a mi lado\nY presumirle a la gente que estoy\nEnamorado\n\nPorque me gustas\nPorque me encantas, porque te amo\nPorque me gustas\nPorque disfruto estar a tu lado\n\nEres divina\nCon tu sonrisa que me fascina\nTe ves tan linda\nQue no hay palabras que te describan\n\nSacrificarme por un beso tuyo\nSería un honor si estás a mi lado\nY presumirle a la gente que estoy\nEnamorado\n\nPorque me gustas\nPorque me encantas, porque te amo\nPorque me gustas\nPorque disfruto estar a tu lado"
     }
 ];
 
